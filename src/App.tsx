fed9a6f0744062d92379dae188d9b445cf2d5f70
Max.li <maxli@jiemogame.com>
2022-12-07 19:28:35 +0800

    fix: meta login

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,28 @@ import Layout from "lib/layout";
 import Routings from "lib/router/Routings";
 import { theme } from "lib/styles/customTheme";
 
+// import { connectors } from "./connectors";
+// https://wagmi.sh/docs/migration-guide#provider 0.2.12
+// <WagmiProvider autoConnect connectors={connectors}></WagmiProvider>
+
+import { WagmiConfig, createClient } from "wagmi";
+import { getDefaultProvider } from "ethers";
+
+const client = createClient({
+  autoConnect: true,
+  // connectors,
+  // provider,
+  provider: getDefaultProvider(),
+});
 const App = () => (
   <ChakraProvider theme={theme}>
-    <Router>
-      <Layout>
-        <Routings />
-      </Layout>
-    </Router>
+    <WagmiConfig client={client}>
+      <Router>
+        <Layout>
+          <Routings />
+        </Layout>
+      </Router>
+    </WagmiConfig>
   </ChakraProvider>
 );
 
